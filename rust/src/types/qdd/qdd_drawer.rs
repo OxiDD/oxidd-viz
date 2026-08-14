c706c9187f9d534358f3a5c6133e079227110a4b
Nils Husung <husung@cs.uni-saarland.de>
2026-08-14 15:15:12 +0200

    Fix capitalization of LaTeX, DDDMP, and BuDDy

--- rust/src/types/qdd/qdd_drawer.rs
+++ rust/src/types/qdd/qdd_drawer.rs
@@ -700,7 +700,7 @@ impl QDDDiagramDrawer {
             ContainerConfig::new(
                 ContainerStyle::new().margin_top(TOP_MARGIN),
                 LabelConfig::new_styled(
-                    "Latex",
+                    "LaTeX",
                     LabelKind::Category,
                     CompositeConfig::new((
                         ButtonConfig::new_labeled("Generate"),
